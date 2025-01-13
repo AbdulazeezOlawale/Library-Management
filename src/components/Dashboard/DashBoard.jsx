976411f6beecd717406e83ac4b69be4250129618
AbdulazeezOlawale <orionabq@gmail.com>
2025-01-13 08:58:58 +0100

    stats

--- src/components/Dashboard/DashBoard.jsx
+++ src/components/Dashboard/DashBoard.jsx
@@ -7,9 +7,25 @@ const DashBoard = () => {
         <div className={`${styles.gridItem} ${styles.item1}`}>1</div>
         <div className={`${styles.gridItem} ${styles.item2}`}>2</div>
         <div className={`${styles.gridItem} ${styles.item3}`}>
-          
+          <div className={styles.item3_container}>
+            {data.record.map((dataItem, index) => (
+              <div className={styles.recordItem} key={index}>
+                <div className={styles.recordItem_icon}>
+                  <img src={dataItem.icon} alt="" />
+                </div>
+
+                <div className={styles.desc}>
+                  <p>{dataItem.heading}</p>
+                  <small>{dataItem.desc}</small>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className={`${styles.gridItem} ${styles.item4}`}>4</div>
+        <div className={`${styles.gridItem} ${styles.item4}`}>
+          <small>BookWorm Admins</small>
+          <img src="./images/vector.png" alt="" />
+        </div>
         <div className={`${styles.gridItem} ${styles.item5}`}>
           <span>
             <img src="./images/3dpie.png" alt="pie" />
@@ -17,7 +33,7 @@ const DashBoard = () => {
         </div>
         <div className={`${styles.gridItem} ${styles.item6}`}>
           <span>
-            <img src="./images/SvgjsG2242.png" alt="" />
+            <img src="./images/building.png" alt="" />
           </span>
 
           <span>
@@ -27,7 +43,7 @@ const DashBoard = () => {
                   style={{ backgroundColor: `${item.color}` }}
                   className={styles.bulletp}
                 ></div>
-                <p>{item.title}</p>
+                <small>{item.title}</small>
               </div>
             ))}
           </span>
